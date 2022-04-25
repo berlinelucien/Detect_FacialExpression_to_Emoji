701d56cf8ded61e3db11254dabb81a5df21ee089
Berlin <Berline_lucien@yahoo.com>
2022-04-24 20:47:08 -0400

    added default webcame , function to show avatar

--- src/emoji.py
+++ src/emoji.py
@@ -1,16 +1,17 @@
 import tkinter as tk
 from tkinter import *
-import cv2
 from PIL import Image, ImageTk
 import os
 import numpy as np
 import cv2
+from keras.layers import Conv2D
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.optimizers import adam_v2
 from keras.layers import MaxPooling2D
-from keras_preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 import threading
+
 
 emotion_model = Sequential()
 emotion_model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(48,48,1)))
@@ -48,23 +49,72 @@ emoji_dict={
 }
 
 global last_frame1
-last_frame1 = np.zeros((480,640,3), dtype=uint8)
+last_frame1=np.zeros((480,640,3), dtype=np.int8)
 global cap1
 show_text=[0]
 global frame_number
 
 
 def show_subject():
-    pass
+    #if i want to add a prepared video specifiy the path or
+    # internal camera just use 0
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    cap.set(10, 100)
+    if not cap.isOpened():
+        print("Can't open the camera")
+    global frame_number
+    length=int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_number += 1
+    if frame_number >= length:
+        exit()
+    cap.set(1, frame_number)
+    flag1, frame1 = cap.read()
+    frame1 = cv2.resize(frame1,(600,500))
+    bounding_box = cv2.CascadeClassifier(
+        "C:/Users/berlin/.virtualenvs/DetectFacialExpression/bin/python/site-packages/cv2/data"
+        "/haarcascade_frontalface_default.xml")
+    gray_frame=cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    num_faces = bounding_box.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+    for (x,y,w,h) in num_faces:
+        cv2.rectangle(frame1, (x,y-50), (x+w, y+h+10), (255,0,0), 2)
+        roi_gray_frame = gray_frame[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame,(48,48)),-1),0)
+        prediction = emotion_model.predict(cropped_img)
+        maxindex = int(np.argmax(prediction))
+        cv2.putText(frame1, emotion_dict[maxindex], (x+20,y-60),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2,cv2.LINE_AA)
+        show_text[0]=maxindex
+    if flag1 is None:
+        print("Major error!")
+    elif flag1:
+        global last_frame1
+        last_frame1 = frame1.copy()
+        pic = cv2.cvtColor(last_frame1, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(pic)
+        imgtk=ImageTk.PhotoImage(image=img)
+        lmain.imgtk = imgtk
+        lmain.configure(image=imgtk)
+        root.update()
+        lmain.after(10, show_subject)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        exit()
 
+def show_avatar():
+    #read the emoji
+    frame2=cv2.imread(emoji_dict[show_text[0]])
+    #get the picture convert it to RGB
+    pic2=cv2.cvtColor(frame2,cv2.COLOR_BGR2RGB)
+    img2=Image.fromarray(frame2)
+    imgtk2=ImageTk.PhotoImage(image=img2)
+    lmain2.imgtk2=imgtk2
+    #configuring the text and font
+    lmain3.configure(text=emotion_dict[show_text[0]],font=('arial',45,'bold'))
 
-
-
-
-
-
-
-
+    lmain2.configure(image=imgtk2)
+    root.update()
+    #call the same function again after 10millisecond
+    lmain2.after(10, show_avatar)
 
 
 if __name__ == "__main__":
